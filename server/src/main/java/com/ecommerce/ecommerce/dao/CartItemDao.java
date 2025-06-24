1fa40e4c230529f623f25801cf9a35e328e9dec7
MuhammedNiyas2003 <nichuniyas454@gmail.com>
2025-06-24 20:50:29 +0530

    adding some methods in cartItemDAo

--- server/src/main/java/com/ecommerce/ecommerce/dao/CartItemDao.java
+++ server/src/main/java/com/ecommerce/ecommerce/dao/CartItemDao.java
@@ -2,10 +2,16 @@ package com.ecommerce.ecommerce.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.ecommerce.ecommerce.model.CartItem;
 import com.ecommerce.ecommerce.repository.CartItemRepository;
 
 @Repository
 public class CartItemDao {
     @Autowired
     private CartItemRepository cartItemRepository;
+
+    public CartItem save(CartItem cartItem){
+        return cartItemRepository.save(cartItem);
+    }
 }
