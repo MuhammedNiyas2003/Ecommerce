13ddf942f5643ae5ae53c31a44ef9b8d24b1fdbf
MuhammedNiyas2003 <nichuniyas454@gmail.com>
2025-07-03 20:14:54 +0530

    minor changes in product controller class

--- server/src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
+++ server/src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,16 +15,16 @@ import com.ecommerce.ecommerce.model.Product;
 import com.ecommerce.ecommerce.response.ResponseStructure;
 import com.ecommerce.ecommerce.service.ProductService;
 
-@RequestMapping("/project")
+@RequestMapping("/product")
 @RestController
 public class ProductController {
     @Autowired
     private ProductService productService;
-    @PostMapping("/product")
+    @PostMapping("/new")
     public ResponseEntity<ResponseStructure<Product>> save(@RequestBody Product product , @PathVariable int sellerId){
         return  productService.save(product, sellerId);
     }
-
+    @GetMapping("/get")
     public ResponseEntity<ResponseStructure<List<Product>>> findBySellerId(@PathVariable Integer id){
         return productService.findBySellerId(id);
     }
