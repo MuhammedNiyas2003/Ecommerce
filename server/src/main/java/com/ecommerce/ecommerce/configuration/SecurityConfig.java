57dea121466cb7507fb4541c303019f722644c86
MuhammedNiyas2003 <nichuniyas454@gmail.com>
2025-07-06 09:46:20 +0530

    changing into builder pattern

--- server/src/main/java/com/ecommerce/ecommerce/configuration/SecurityConfig.java
+++ server/src/main/java/com/ecommerce/ecommerce/configuration/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.ecommerce.ecommerce.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,13 +13,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf(customizer ->customizer.disable()); 
-        //Disabling the CSRF
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        //configuring that whenever we are requesting it should as for authorization
-        http.httpBasic(Customizer.withDefaults());
-        //making the configuration for accessing api request 
-        return http.build();
+        return http
+            .csrf(customizer ->customizer.disable())
+             //Disabling the CSRF
+            .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+             //configuring that whenever we are requesting it should as for authorization
+            .httpBasic(Customizer.withDefaults())
+             //making the configuration for accessing api request 
+            .build();
     }
 
 }
