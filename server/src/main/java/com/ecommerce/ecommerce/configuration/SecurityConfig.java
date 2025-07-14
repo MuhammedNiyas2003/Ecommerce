4c65a39840bb3417f19eb731e3354d6bf9b7f440
MuhammedNiyas2003 <nichuniyas454@gmail.com>
2025-07-14 20:09:55 +0530

    minor change

--- server/src/main/java/com/ecommerce/ecommerce/configuration/SecurityConfig.java
+++ server/src/main/java/com/ecommerce/ecommerce/configuration/SecurityConfig.java
@@ -2,28 +2,33 @@ package com.ecommerce.ecommerce.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .csrf(customizer ->customizer.disable())
-             //Disabling the CSRF
-            .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-             //configuring that whenever we are requesting it should as for authorization
-            .httpBasic(Customizer.withDefaults())
-             //making the configuration for accessing api request 
-            .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-             //making the session new for each request 
-            .build();
+                .csrf(customizer -> customizer.disable())
+                // Disabling the CSRF
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                // configuring that whenever we are requesting it should as for authorization
+                .httpBasic(Customizer.withDefaults())
+                // making the configuration for accessing api request
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // making the session new for each request
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return null;
     }
 
 }
